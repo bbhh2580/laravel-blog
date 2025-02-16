c2bfbca9b90e39189b3b909981ed53acb4f42fdc
Hana <fweriuhb@gmail.com>
2025-02-16 18:56:34 +0900

    Create SessionsController.php

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Redirector;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+
+
+class SessionsController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
+
+    /**
+     * Show the form for login
+     *
+     * @return Factory|View|Application
+     */
+
+
+}
